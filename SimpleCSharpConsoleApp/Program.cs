0bcb7588acd96eb54aa6e9decafc1fca1a55e661
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-13 10:33:30 -0400

    Initial commit

--- SimpleCSharpConsoleApp/Program.cs
+++ SimpleCSharpConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SimpleCSharpConsoleApp
 {
@@ -10,6 +11,17 @@ namespace SimpleCSharpConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*************");
+            Console.WriteLine("Welcome to My rocking App");
+            Console.WriteLine("*************");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            // Wait for Enter key to be pressed.
+            Console.ReadLine();
+            MessageBox.Show("All done!");
         }
     }
 }
