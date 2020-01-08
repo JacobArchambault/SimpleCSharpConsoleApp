559ea8600055f47620ac4c558d988f78558b5e18
Jacob Archambault <jacob.archambault@waystar.com>
2020-01-08 08:23:37 -0500

    Remove unused usings from program class and args parameter from main method.

--- SimpleCSharpConsoleApp/Program.cs
+++ SimpleCSharpConsoleApp/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SimpleCSharpConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.Title = "My Rocking App";
             Console.ForegroundColor = ConsoleColor.Yellow;
